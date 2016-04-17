222b2cdc6630425b3de6f85a7cd809e6f7e65367
Junior <fabienjunior.fresse@gmail.com>
2016-04-18 00:32:14 +0200

    exemple SDI

--- test/fenprincipale.cpp
+++ test/fenprincipale.cpp
@@ -2,5 +2,18 @@
 
 FenPrincipale::FenPrincipale()
 {
+    QWidget *zoneCentrale = new QWidget;
 
+    QLineEdit *nom = new QLineEdit;
+    QLineEdit *prenom = new QLineEdit;
+    QLineEdit *age = new QLineEdit;
+
+    QFormLayout *layout = new QFormLayout;
+    layout->addRow("Votre nom", nom);
+    layout->addRow("Votre prénom", prenom);
+    layout->addRow("Votre âge", age);
+
+    zoneCentrale->setLayout(layout);
+
+    setCentralWidget(zoneCentrale);
 }
